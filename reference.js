2bad94dde7a7cbdba65529b8c5a7e10dddce258b
bakour <81446233+bakour1@users.noreply.github.com>
2021-06-20 06:52:23 +0100

    Add files via upload

--- reference.js
+++ reference.js
@@ -402,8 +402,8 @@ console.assert(2 != 3 ,'the # is not even'); // not error no msg no thing
     console.log( 100..toString() ); //100 ""
     console.log( 100.1.toString() ); //100.1 ""
 
-    console.log( 100.55555.toFixed(2) ); //100.56 ""
-    console.log( 100.55455.toFixed( 2 ) ); //100.55 ""
+    console.log( 100.55555.toFixed(2) ); //100.56 
+    console.log( 100.55455.toFixed( 2 ) ); //100.55 
 
     console.log( Number( "100 osama" ) ); //nan $
     console.log( Number( +"100 osama" ) ); //nan $
@@ -490,4 +490,30 @@ console.assert(2 != 3 ,'the # is not even'); // not error no msg no thing
 
     // assign 7---------------------------
     console.log(Math.floor((Math.random() * 5) + 0) ); // 0 || 1 || 2 || 3 || 4
+}
+// Challenge
+{
+    let a = 1_00;
+    let b = 2_00.55;
+    let c = 1e2;
+    let d = 2.4;
+
+    //find smallest number in all variable
+    console.log( Math.trunc( Math.min(a, b, c, d)) );  // 2
+
+    // use variable a + b one time to get the needed output
+    console.log( d%d + Math.pow(a, 2) );            // 10000
+    console.log(Math.pow(a, 2) * Math.pow(d, 0))    // 10000
+    console.log( Math.pow( a, Math.trunc( d ) ) );  // 10000
+
+    //get  integer "2" from d variable with methods
+    console.log( Math.trunc( d ) );       // 2
+    console.log( Math.floor( d ) );       // 2
+    console.log( Math.round( d ) );       // 2
+    console.log( Math.ceil( d ) - +true ); // 2
+
+    // use variables b + d to get this values 
+
+    console.log( ( Math.floor( b ) / Math.ceil( d ) ).toFixed( 2 ) );        // 66.67 => string
+    console.log( Math.round( ( Math.floor( b ) / Math.ceil( d ) ) ) );       // 67    => number
 }
